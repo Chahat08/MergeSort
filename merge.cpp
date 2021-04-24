c48d08e19f3e0ba2b69d298143b0366e3f5b4605
Chahat Kalsi <chahat.ck88@gmail.com>
2021-04-24 09:01:33 +0530

    modified the merge operation for vectors

--- merge.cpp
+++ merge.cpp
@@ -1,28 +1,29 @@
 #include <iostream>
 #include <cstdlib>
+#include <vector>
 
 using std::cout; using std::endl;
+using std::vector;
 
-void merge(int* a, int* b, int* c, int m, int n)
+void merge(vector<int>& a, vector<int>& b, vector<int>& c)
 {
-	for(int k=0, i=0, j=0; k<m+n; ++k)
+	for(int k=0, i=0, j=0; k<a.size()+b.size(); ++k)
 	{
-		if(i==m) {c[k]=(b[j++]);continue;}
-		if(j==n) {c[k]=(a[i++]);continue;}
-		c[k] =  a[i] < b[j] ? (a[i++]) : (b[j++]);
+		if(i==a.size()) {c.push_back(b[j++]);continue;}
+		if(j==b.size()) {c.push_back(a[i++]);continue;}
+		a[i] < b[j] ? c.push_back(a[i++]) : c.push_back(b[j++]);
 	}
 }
 
 int main()
 {	
-	int a[] = {3, 19, 25, 87, 96};
-	int b[] = {2, 25, 27, 31, 43, 59, 66, 72, 80, 98, 103};
+	vector<int> a = {3, 19, 25, 87, 96};
+	vector<int> b = {2, 25, 27, 31, 43, 59, 66, 72, 80, 98, 103};
 
-	int m = sizeof(a)/sizeof(a[0]), n = sizeof(b)/sizeof(b[0]);
-	int* c = new int[m+n];
+	vector<int> c;
 
-	merge(a, b, c, m, n);
-	for(int i = 0; i<m+n; ++i) cout<<c[i]<<endl;
+	merge(a, b, c);
+	for(int i = 0; i<c.size(); ++i) cout<<c[i]<<endl;
 
 	return 0;
 }
